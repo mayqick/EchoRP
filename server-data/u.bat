331b41f4051a25c1ce2071050fc1fbe3158bb2c6
Oniel <51058739+0niel@users.noreply.github.com>
2020-01-25 13:28:53 +0300

    Update u.bat

--- server-data/u.bat
+++ server-data/u.bat
@@ -1,3 +1,3 @@
-cd /d C:\Users\Oniel\Documents\GitHub\fivem_echo\server-data
-"C:\Users\Oniel\Documents\GitHub\fivem_echo\run.cmd" +exec server.cfg +set onesync_enabled 1
+cd /d C:\Users\Oniel\Documents\GitHub\EchoRP_To_Merge\server-data
+"C:\Users\Oniel\Documents\GitHub\EchoRP_To_Merge\run.cmd" +exec server.cfg +set onesync_enabled 1
 pause
